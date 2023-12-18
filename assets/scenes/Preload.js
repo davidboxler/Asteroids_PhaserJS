bb3458d4e3f8fe8755e486f19af65a6dc4ae0a8f
davidboxler <davidboxler47@gmail.com>
2023-12-18 16:57:52 -0300

    Se agregan cambios carpetas y rutas

--- assets/scenes/Preload.js
+++ assets/scenes/Preload.js
@@ -8,57 +8,57 @@ export default class Preload extends Phaser.Scene {
 
   preload() {
     /* ---------- IMAGENES NAVES ----------- */
-    this.load.image("ship1", "../../assets/images/nave.PNG");
+    this.load.image("ship1", "./assets/images/nave.PNG");
 
     /* ---------- IMAGENES FONDO ----------- */
-    this.load.image("fondo", ".../../assets/images/fondo2.jpg");
+    this.load.image("fondo", "./assets/images/fondo2.jpg");
 
     /* ---------- IMAGENES DISPAROS ----------- */
-    this.load.image("bullet", "../../assets/images/bullet.png");
+    this.load.image("bullet", "./assets/images/bullet.png");
 
     /* ---------- IMAGENES BOTONES VIDEOJUEGO ----------- */
-    this.load.image("btn_start", "../../assets/images/btn_Start2.png");
-    this.load.image("btn_next", "../../assets/images/btn_next.png");
-    this.load.image("btn_sound", "../../assets/images/btn_sound2.png");
-    this.load.image("btn_notSound", "../../assets/images/btn_notSound2.png");
-    this.load.image("btn_pause", "../../assets/images/btn_pause2.png");
-    this.load.image("btn_play", "../../assets/images/btn_play2.png");
-    this.load.image("btn_help", "../../assets/images/btn_help2.png");
-    this.load.image("btn_salir", "../../assets/images/btn_Salir.png");
+    this.load.image("btn_start", "./assets/images/btn_Start2.png");
+    this.load.image("btn_next", "./assets/images/btn_next.png");
+    this.load.image("btn_sound", "./assets/images/btn_sound2.png");
+    this.load.image("btn_notSound", "./assets/images/btn_notSound2.png");
+    this.load.image("btn_pause", "./assets/images/btn_pause2.png");
+    this.load.image("btn_play", "./assets/images/btn_play2.png");
+    this.load.image("btn_help", "./assets/images/btn_help2.png");
+    this.load.image("btn_salir", "./assets/images/btn_Salir.png");
 
     /* ---------- IMAGENES INSTRUCCIONES ----------- */
-    this.load.image("box_shoter", "../../assets/images/disparo_ast.png");
-    this.load.image("box_shoter_exp", "../../assets/images/disparo_exp.png");
-    this.load.image('box_move', '../../assets/images/movimiento.png');
+    this.load.image("box_shoter", "./assets/images/disparo_ast.png");
+    this.load.image("box_shoter_exp", "./assets/images/disparo_exp.png");
+    this.load.image('box_move', './assets/images/movimiento.png');
 
     /* ---------- AUDIOS VIDEOJUEGO ----------- */
-    this.load.audio("laserPlayer", "../../assets/audio/laser.ogg");
-    this.load.audio("laserEnemy", "../../assets/audio/enemyShoot.wav");
-    this.load.audio("explosion", "../../assets/audio/explode.wav");
-    this.load.audio("soundTrack", "../../assets/audio/background.ogg");
+    this.load.audio("laserPlayer", "./assets/audio/laser.ogg");
+    this.load.audio("laserEnemy", "./assets/audio/enemyShoot.wav");
+    this.load.audio("explosion", "./assets/audio/explode.wav");
+    this.load.audio("soundTrack", "./assets/audio/background.ogg");
 
     /* ---------- SPRITESHEET VIDEOJUEGO ----------- */
-    this.load.spritesheet("rocks", "../../assets/images/rocks.png", {
+    this.load.spritesheet("rocks", "./assets/images/rocks.png", {
       frameWidth: 125,
       frameHeight: 100,
     });
-    this.load.spritesheet("exp", "./../assets/images/exp.png", {
+    this.load.spritesheet("exp", "./assets/images/exp.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
-    this.load.spritesheet("effect", "../../assets/images/sprite.png", {
+    this.load.spritesheet("effect", "./assets/images/sprite.png", {
       frameWidth: 100,
       frameHeight: 100,
     });
 
     /* ---------- IMAGENES FINALES VIDEOJUEGO ----------- */
-    this.load.image("game-over", "../../assets/images/game-over.png");
-    this.load.image("win", "../../assets/images/winner.png");
-    this.load.image("title", "../../assets/images/titulo.png")
+    this.load.image("game-over", "./assets/images/game-over.png");
+    this.load.image("win", "./assets/images/winner.png");
+    this.load.image("title", "./assets/images/titulo.png")
 
     /* --------------- CINEMATICAS ------------------- */
-    this.load.video("cinematicaInicial", "../../assets/images/cinematica.mp4")
-    this.load.video("creditos", "../../assets/images/creditos.mp4")
+    this.load.video("cinematicaInicial", "./assets/images/cinematica.mp4")
+    this.load.video("creditos", "./assets/images/creditos.mp4")
   }
 
   create() {
---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+import Cinematica from "../assets/scenes/Cinematica.js";
+import Creditos from "../assets/scenes/Creditos.js";
+import GameOver from "../assets/scenes/GameOver.js";
+import Help from "../assets/scenes/Help.js";
+import Level1 from "../assets/scenes/Level1.js";
+import Level2 from "../assets/scenes/Level2.js";
+import Level3 from "../assets/scenes/Level3.js";
+import Menu from "../assets/scenes/Menu.js";
+import Preload from "../assets/scenes/Preload.js";
+import Winner from "../assets/scenes/Winner.js";
+
+// Crea un nuevo objeto de configuración de Phaser
+const config = {
+  type: Phaser.AUTO,
+  width: 640, // Ancho fijo para el diseño vertical
+  height: 960, // Alto fijo para el diseño vertical
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      // gravity: { y: 200 },
+      debug: false,
+    },
+  },
+  // Lista de escenas para cargar
+  // Solo la primera escena se mostrará
+  // Recuerda importar la escena antes de agregarla a la lista
+  scene: [
+    Preload,
+    Level1,
+    Level2,
+    Level3,
+    Winner,
+    GameOver,
+    Menu,
+    Help,
+    Creditos,
+    Cinematica,
+  ],
+};
+
+// Crea una nueva instancia de juego de Phaser
+window.game = new Phaser.Game(config);
